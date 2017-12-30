980cc92677051d80085bef82fbdd1c1dca6d9249
Wu Xiaodong <33693065+xiaodongww@users.noreply.github.com>
2017-12-30 14:26:39 +0800

    Update plotTensorImage.py

--- plotTensorImage.py
+++ plotTensorImage.py
@@ -1,4 +1,20 @@
 # plot a tenosr 
+
+#Method 1
 def show(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
+    
+ 
+# Method 2
+import torch as t
+from torchvision.transforms import ToPILImage
+from IPython.display import Image
+to_img = ToPILImage()
+
+# display tensor
+a = t.Tensor(3, 64, 64).normal_()
+to_img(a)
+
+# display imagefile
+Image('/path/to/my.png')
